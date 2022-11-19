e1c9b2bde4e6be283e14e01f70ef73228c6f270b
RoshantheGreat <113701114+RoshantheGreat@users.noreply.github.com>
2022-11-19 10:36:49 +0530

    Update App.py

--- Project Design & Planning/Final Deliverables/App.py
+++ Project Design & Planning/Final Deliverables/App.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("home.html")
+    return render_template("HTML code 1.html")
 
 
 @app.route('/predict',methods=['POST'])
@@ -13,7 +13,7 @@ def predict():
     if request.method=='POST':
       image = request.files.get('photo', '')
       best, others, img_name = recognize(image)
-      return render_template("predict.html", best=best, others=others, img_name=img_name)
+      return render_template("HTML code 2.html", best=best, others=others, img_name=img_name)
 
 
 if __name__=="__main__":
